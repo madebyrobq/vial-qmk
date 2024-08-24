1faca96cc8132ae76fef9191c47e3dc55c335f12
Rob Quinn <45523916+madebyrobq@users.noreply.github.com>
2024-08-24 11:48:16 -0700

    top level , and .

--- keyboards/madebyrobq/ortho4x10tkl/keymaps/vial/keymap.c
+++ keyboards/madebyrobq/ortho4x10tkl/keymaps/vial/keymap.c
@@ -30,14 +30,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┤┌       ┌───────┐       ┐
      * │  Z(⇧) │   X   │   C   │   V   │   B   │   N   │  M(⇧) │  PG DN │   Up  │ PG UP
      * ├───────┼───────┼───────┴───────┴───────┼───────┼───────┤┌───────┼───────┼───────┐
-     * │   ⌥   │   ⌘   │         Space         │SPC/RSE│SPC/RSE││  Left │  Down │ Right │
+     * │   ⌥   │   ⌘   │         Space         │   ,   │   .   ││  Left │  Down │ Right │
      * └───────┴───────┴───────────────────────┴───────┴───────┘└───────┴───────┴───────┘
      */
     [DEFAULT_LAYER] = LAYOUT_ortho_4x10(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KCZ_SFT, KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KCM_SFT, KC_PGDN, KC_UP,   KC_PGUP,
-        KC_LALT, KC_LCMD, KC_SPC,  KC_SPC,  KC_SPC,  SPC_RSE, SPC_RSE, KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_LALT, KC_LCMD, KC_SPC,  KC_SPC,  KC_SPC,  KC_COMM, KC_DOT,  KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
     [RAISE_LAYER] = LAYOUT_ortho_4x10(
